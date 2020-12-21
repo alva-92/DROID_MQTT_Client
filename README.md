12bc17bce7651dd90973f028af773e0e19883f79
Gerardo Enrique Alvarenga <alva.92@outlook.com>
2020-12-20 23:00:23 -0500

    Added SSL connection base

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
 # DROID_MQTT_Client
+
+## MQTT
+
+MQTT works as Pub-Sub. MQTT uses very small bytes of data to express the details of the content being transported.
+In general, MQTT is more suitable for IOT projects because of its reliability and small header footprint.
+
+
+# Requirements
+
++ Mosquitto
+Install Paho MQTT
+Once install open a terminal and navigate to the installation folder and run the following command to start the subscriber
+
+```bat
+mosquitto_sub -t topic/DroidSample
+```
+
+mosquitto_sub -t topic/TestTopic -v
+
+mosquitto_pub -t ResponseTopic -m 'Damn'
+
+## Setup
+
++ Add the following to the build gradle 
+
+repositories {
+    jcenter()
+    maven {
+        url "https://repo.eclipse.org/content/repositories/paho-snapshots/"
+    }
+}
+
++ Add the following to the app gradle dependencies
+
+compileOnly 'com.google.android.things:androidthings:0.1-devpreview'
+implementation 'org.eclipse.paho:org.eclipse.paho.client.mqttv3:1.0.2'
+
++ Sample implementation on Main Activity
+
+try {
+    MqttClient client = new MqttClient("tcp://192.168.1.5:1883", "AndroidThingSub", new MemoryPersistence());
+    client.setCallback(this);
+    client.connect();
+    String topic = "topic/ayushgemini";
+    client.subscribe(topic);
+} catch (MqttException e) {
+    e.printStackTrace();
+}
+
+
+## Resources
+
+https://medium.com/@gaikwadchetan93/android-real-time-communication-using-mqtt-9ea42551475d
--- app/src/main/java/com/alvarenga/enrique/droid_mqtt_client/MainActivity.java
+++ app/src/main/java/com/alvarenga/enrique/droid_mqtt_client/MainActivity.java
@@ -3,20 +3,38 @@ package com.alvarenga.enrique.droid_mqtt_client;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.IMqttToken;
 import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.security.KeyStore;
+import java.security.Security;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManagerFactory;
+
 public class MainActivity extends AppCompatActivity implements MqttCallback {
 
+    /* Class logging TAG */
     private static final String TAG = "MainActivity" ;
 
     @Override
@@ -24,8 +42,15 @@ public class MainActivity extends AppCompatActivity implements MqttCallback {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+    }
+
+    private void openMqttConnection(){
         try {
-            MqttClient client = new MqttClient("tcp://192.168.0.29:1883", "AndroidSub", new MemoryPersistence());
+            String serverURI = "tcp://192.168.0.71:1883";
+
+            MqttClient client = new MqttClient(serverURI, "pulseDroid", new MemoryPersistence());
+
             client.setCallback(this);
             client.connect();
             String topic = "topic/DroidSample";
@@ -37,12 +62,96 @@ public class MainActivity extends AppCompatActivity implements MqttCallback {
             } catch (MqttException e) {
                 e.printStackTrace();
             }
-            //initMqtt();
         } catch (MqttException e) {
             e.printStackTrace();
         }
     }
 
+    private void sslMqttConnection(){
+
+        String caFilePath =  Environment.getExternalStorageDirectory().getAbsolutePath() +
+                "/DroidMQTT/" + "ca.crt";
+
+        try {
+            String serverURI = "ssl://192.168.0.71:8883";
+
+            MqttClient client = new MqttClient(serverURI, "DroidClient", new MemoryPersistence());
+
+            MqttConnectOptions options = new MqttConnectOptions();
+            SSLSocketFactory socketFactory = getSocketFactory(caFilePath);
+            options.setSocketFactory(socketFactory);
+
+            client.setCallback(this);
+            client.connect(options);
+            String topic = "topic/DroidSample";
+            client.subscribe(topic);
+
+            MqttMessage message = new MqttMessage("Android Active".getBytes());
+            try {
+                client.publish("topic/OnlyDroid", message);
+            } catch (MqttException e) {
+                e.printStackTrace();
+            }
+        } catch (MqttException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private SSLSocketFactory getSocketFactoryUsingBKS(final String caCrtFile)
+            throws Exception {
+
+        KeyStore trustStore = KeyStore.getInstance("BKS");
+
+        InputStream input = this.getResources().openRawResource(R.raw.mystore);
+        trustStore.load(input, null);
+
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+        tmf.init(trustStore);
+
+        SSLContext sslCtx = SSLContext.getInstance("TLS");
+        sslCtx.init(null, tmf.getTrustManagers(), null);
+
+        return sslCtx.getSocketFactory();
+    }
+
+    private SSLSocketFactory getSocketFactory(final String caCrtFile)
+            throws Exception {
+
+        Security.addProvider(new BouncyCastleProvider());
+
+        // Load CAs from an InputStream
+        CertificateFactory cf = CertificateFactory.getInstance("X.509");
+        File file = new File(caCrtFile);
+        InputStream caInput = new BufferedInputStream(new FileInputStream(file));
+        Certificate ca;
+        try {
+            ca = cf.generateCertificate(caInput);
+            Log.d(TAG, "getSocketFactory: ca=" + ((X509Certificate) ca).getSubjectDN());
+            Log.d(TAG, "getSocketFactory: caFull=" + ca.toString());
+        } finally {
+            caInput.close();
+        }
+
+        // Create a KeyStore containing our trusted CAs
+        String keyStoreType = KeyStore.getDefaultType();
+        KeyStore keyStore = KeyStore.getInstance(keyStoreType);
+        keyStore.load(null, null);
+        keyStore.setCertificateEntry("ca", ca);
+
+        // Create a TrustManager that trusts the CAs in our KeyStore
+        String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
+        tmf.init(keyStore);
+
+        // Create an SSLContext that uses our TrustManager
+        SSLContext context = SSLContext.getInstance("TLS");
+        context.init(null, tmf.getTrustManagers(), null);
+
+        return context.getSocketFactory();
+    }
     @Override
     public void connectionLost(Throwable cause) {
         Log.d(TAG, "connectionLost");
