5b6b26a09aa1ccab3777f477b5f53197a133dff5
Gerardo Enrique Alvarenga <alva.92@outlook.com>
2020-12-28 10:39:07 -0500

    SSL context added

--- app/src/main/java/com/alvarenga/enrique/droid_mqtt_client/MainActivity.java
+++ app/src/main/java/com/alvarenga/enrique/droid_mqtt_client/MainActivity.java
@@ -1,7 +1,11 @@
 package com.alvarenga.enrique.droid_mqtt_client;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
@@ -42,6 +46,8 @@ public class MainActivity extends AppCompatActivity implements MqttCallback {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        isReadStoragePermissionGranted();
+        sslMqttConnection();
 
     }
 
@@ -70,7 +76,7 @@ public class MainActivity extends AppCompatActivity implements MqttCallback {
     private void sslMqttConnection(){
 
         String caFilePath =  Environment.getExternalStorageDirectory().getAbsolutePath() +
-                "/DroidMQTT/" + "ca.crt";
+                "/DROID_MQTT/" + "ca.crt";
 
         try {
             String serverURI = "ssl://192.168.0.71:8883";
@@ -83,12 +89,12 @@ public class MainActivity extends AppCompatActivity implements MqttCallback {
 
             client.setCallback(this);
             client.connect(options);
-            String topic = "topic/DroidSample";
+            String topic = "topic/SSLsub";
             client.subscribe(topic);
 
-            MqttMessage message = new MqttMessage("Android Active".getBytes());
+            MqttMessage message = new MqttMessage("Android SSL Message".getBytes());
             try {
-                client.publish("topic/OnlyDroid", message);
+                client.publish("topic/SSLTest", message);
             } catch (MqttException e) {
                 e.printStackTrace();
             }
@@ -105,7 +111,7 @@ public class MainActivity extends AppCompatActivity implements MqttCallback {
 
         KeyStore trustStore = KeyStore.getInstance("BKS");
 
-        InputStream input = this.getResources().openRawResource(R.raw.mystore);
+        InputStream input = getResources().openRawResource(R.raw.droidstore);
         trustStore.load(input, null);
 
         TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
@@ -126,17 +132,19 @@ public class MainActivity extends AppCompatActivity implements MqttCallback {
         CertificateFactory cf = CertificateFactory.getInstance("X.509");
         File file = new File(caCrtFile);
         InputStream caInput = new BufferedInputStream(new FileInputStream(file));
-        Certificate ca;
+        Certificate ca = null;
         try {
-            ca = cf.generateCertificate(caInput);
+            ca = cf.generateCertificate(caInput); // Convert crt to Certificate Java Object
             Log.d(TAG, "getSocketFactory: ca=" + ((X509Certificate) ca).getSubjectDN());
             Log.d(TAG, "getSocketFactory: caFull=" + ca.toString());
-        } finally {
+        } catch (Exception e){
+            Log.d(TAG, "getSocketFactory: " + e);
+        }
+        finally {
             caInput.close();
         }
-
         // Create a KeyStore containing our trusted CAs
-        String keyStoreType = KeyStore.getDefaultType();
+        String keyStoreType = KeyStore.getDefaultType(); // default is JKS
         KeyStore keyStore = KeyStore.getInstance(keyStoreType);
         keyStore.load(null, null);
         keyStore.setCertificateEntry("ca", ca);
@@ -211,4 +219,52 @@ public class MainActivity extends AppCompatActivity implements MqttCallback {
             e.printStackTrace();
         }
     }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 2:
+                Log.d(TAG, "Write Permission");
+                if(grantResults[0]== PackageManager.PERMISSION_GRANTED){
+                    Log.v(TAG,"Permission: "+permissions[0]+ "was "+grantResults[0]);
+                }else {
+                    /* Alert that permission is not granted */
+                }
+                break;
+
+            case 3:
+                Log.d(TAG, "Read Permission");
+                if(grantResults[0]== PackageManager.PERMISSION_GRANTED){
+                    Log.v(TAG,"Permission: "+permissions[0]+ "was "+grantResults[0]);
+                }else {
+                    /* Alert that permission is not granted */
+                }
+                break;
+        }
+    }
+
+    public  boolean isReadStoragePermissionGranted() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                Log.v(TAG,"Read Permission is granted");
+                return true;
+            } else {
+
+                Log.v(TAG,"Read Permission is revoked");
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 3);
+                return false;
+            }
+        }
+        else {
+            /* Permission is automatically granted on sdk<23 upon installation */
+            Log.v(TAG,"Read Permission is granted");
+            return true;
+        }
+    }
+
+
 }
